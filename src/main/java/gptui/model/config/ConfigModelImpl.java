23a319adaab10f3e1e7ba14a2ed60aa8b6e7584b
Aleksey Yablokov <alex_ya@mailbox.org>
2025-11-18 00:27:31 +0800

    ConfigModel uses FileSystem

--- src/main/java/gptui/model/config/ConfigModelImpl.java
+++ src/main/java/gptui/model/config/ConfigModelImpl.java
@@ -1,19 +1,21 @@
 package gptui.model.config;
 
+import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 
 import java.io.IOException;
+import java.nio.file.FileSystem;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Properties;
 
 @Singleton
 class ConfigModelImpl implements ConfigModel {
     private final Properties properties = new Properties();
 
-    public ConfigModelImpl() {
+    @Inject
+    public ConfigModelImpl(FileSystem fileSystem) {
         try {
-            var configPath = Path.of(System.getProperty("user.home"), ".gpt", "config.properties");
+            var configPath = fileSystem.getPath(System.getProperty("user.home"), ".gpt", "config.properties");
             properties.load(Files.newInputStream(configPath));
         } catch (IOException e) {
             throw new RuntimeException(e);
